423ed262d05eb10e5ea0ba0010d9a7e507d27920
nkand84 <nityabhagi@gmail.com>
2018-03-18 21:02:12 -0700

    adding image effects on portfolio page

---
+++ assets/javascript/script.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+//     $('.img-effect').on('mouseenter', event => {
+//        $(event.currentTarget).addClass('.photo-active');
+//     });
+//     $('.img-effect').on('mouseleave', event => {
+//         $(event.currentTarget).removeClass('.photo-active');
+//      });
+    
+// });
